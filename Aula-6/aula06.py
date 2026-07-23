d448d79f7eb1f9c0791a6a76866909bed192c7f0
douglasraguiar <douglasaguiar@gmail.com>
2026-07-23 19:31:32 -0300

    Add files via upload

---
+++ Aula-6/aula06.py
@@ -0,0 +1,44 @@
+# Exercícios com variáveis / print() / input()(opcional):
+
+# Calcule o módulo ( % ) de 17 por 4.
+resultado_modulo = 17 % 4
+
+# Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado.
+resultado_multiplicacao = (9/3) ** 2
+
+# Calcule a raiz quadrada de 81.
+resultado_raiz = 81 ** 0.5
+
+# Adicione 20 a 3 vezes 4.
+resultado_adicao = (3 * 4) + 20
+
+# Multiplique 15 por 2 e, em seguida, subtraia 7.
+resultado_multiplicacao_sub = ( 15 * 2 ) - 7
+
+# Eleve 5 ao cubo.
+resultado_cubo = 5 ** 3
+
+# Calcule a média de 17, 21 e 25.
+resultado_media = 17 * 21 * 25
+
+# Multiplique 11 por 2 e adicione 7.
+resultado_multiplicacao_add = (11 * 2) + 7
+
+# Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+resultado_multi_sub = ((3 * 8) - 15) / 2
+
+# Eleve 2 à potência de 10.
+resultado_elevado10 = 2 ** 10
+
+print(
+    "1 - Calcule o módulo ( % ) de 17 por 4:",resultado_modulo,
+    "\n2 - Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado:", resultado_multiplicacao,
+    "\n3 - Calcule a raiz quadrada de 81:",resultado_raiz,
+    "\n4 - Adicione 20 a 3 vezes 4:",resultado_adicao,
+    "\n5 - Multiplique 15 por 2 e, em seguida, subtraia 7:",resultado_multiplicacao_sub,
+    "\n6 - Eleve 5 ao cubo:",resultado_cubo,
+    "\n7 - Calcule a média de 17, 21 e 25:",resultado_media,
+    "\n8 - Multiplique 11 por 2 e adicione 7:",resultado_multiplicacao_add,
+    "\n9 - Subtraia 15 de 3 vezes 8 e divida o resultado por 2:",resultado_multi_sub,
+    "\n10 - Eleve 2 à potência de 10:",resultado_elevado10
+    )
